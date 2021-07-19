f622fecdf56108a6e04e91b3061cff1af4de977b
Johnson <71199326+JL2018-cell@users.noreply.github.com>
2021-07-19 17:10:08 +0800

    Add a new program: Find the k Closest Numbers in a Sorted Array

---
+++ k_close2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* k_close (int * nums, int x, int k, int len) {
+ printf("Lens = %d\n", len);
+ int left = 0;
+ int right = len - k;
+ int mid;
+ while (left < right) {
+  mid = (left + right) / 2;
+  if ((x - nums[mid]) > (nums[mid + k] - x)) {
+   left = mid + 1;
+  }
+  else {
+   right = mid;
+  }
+ } 
+ printf("Left = %d\n",left);
+ printf("num = %d\n",nums);
+ return (nums + left);
+}
+
+int main() {
+ int nums[] = {1,2,3,4,5,6,7,8,9,10,11,12};
+ int i;
+ int * p;
+ printf("num = %d\n",nums);
+ p = k_close(nums, 7, 2, sizeof(nums)/sizeof(int));
+ printf("num = %d\n",nums);
+ printf("*p = %d\n",*p);
+ printf("\n");
+ for (i = 0;i < 2;i++) {
+  printf("%d, ",*(p+i));
+ }
+ printf("\n");
+ return 0;
+}
